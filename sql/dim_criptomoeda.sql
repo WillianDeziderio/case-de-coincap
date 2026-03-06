d9093cccbc9f47efa156ef0cde42eb4f0acebee0
WillianDeziderio <willdeziderio@hotmail.com>
2026-03-05 22:26:34 -0300

    Create dim_criptomoeda.sql

---
+++ sql/dim_criptomoeda.sql
@@ -0,0 +1,7 @@
+-- DDL para a Tabela Dimensão (Cadastro)
+CREATE TABLE IF NOT EXISTS `case-de-specialist-gb.crypto_market_data.dim_criptomoeda` (
+    id STRING OPTIONS(description="Identificador único da criptomoeda na API CoinCap"),
+    symbol STRING OPTIONS(description="Símbolo de negociação (ex: BTC, ETH)"),
+    name STRING OPTIONS(description="Nome completo da criptomoeda"),
+    explorer STRING OPTIONS(description="URL do explorador do blockchain")
+);
