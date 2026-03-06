956270e9b42c7d05953c812ca0d2c9ba658289ab
WillianDeziderio <willdeziderio@hotmail.com>
2026-03-05 22:37:03 -0300

    Update dim_criptomoeda.sql

--- sql/dim_criptomoeda.sql
+++ sql/dim_criptomoeda.sql
@@ -1,5 +1,5 @@
 -- DDL para a Tabela Dimensão (Cadastro)
-CREATE TABLE IF NOT EXISTS `case-de-specialist-gb.crypto_market_data.dim_criptomoeda` (
+CREATE TABLE IF NOT EXISTS `case-de-specialist-gb.raw_crypto_market_data.dim_criptomoeda` (
     id STRING OPTIONS(description="Identificador único da criptomoeda na API CoinCap"),
     symbol STRING OPTIONS(description="Símbolo de negociação (ex: BTC, ETH)"),
     name STRING OPTIONS(description="Nome completo da criptomoeda"),
